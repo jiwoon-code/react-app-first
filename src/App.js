1b652772eab25d9dce50f5723ba343c539023b80
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-03-12 23:56:36 +0900

    themes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
 import styled, { keyframes } from "styled-components";
 
-const Wrapper = styled.div`
-  display: flex;
+const Title = styled.h1`
+  font-size: 40px;
+  color: ${(props) => props.theme.textColor};
 `;
 
-const animation = keyframes`
-0% {
-transform: rotate(0deg);
-border-radius: 0px;
-}
-50%{
-  transform: rotate(360deg);
-  border-radius: 100px;
-}
-100%{
-  transform: rotate(0deg);
-border-radius: 0px;
-}
-`;
-const Box = styled.div`
+const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
-  animation: ${animation} 1s linear infinite;
-  span {
-    font-size: 40px;
-  }
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <span>Hi</span>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
